a0ae4c8e4966861edb554b521f58645713eb05ab
YERRAM RAMA KOTI REDDY <ramakotireddy912@gmail.com>
2024-07-25 21:39:14 +0530

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApi: {
+    baseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+    headers: {
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+      'X-RapidAPI-Key': '4720593e57msh3abdd3843c498c8p11c153jsn6dba63cf1c8e',
+    },
+  },
 };
